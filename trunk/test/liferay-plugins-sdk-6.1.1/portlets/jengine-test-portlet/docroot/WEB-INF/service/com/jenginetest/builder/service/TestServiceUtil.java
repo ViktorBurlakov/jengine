b90609de30a6ad586bc9cff006b5add3efbe8461
vmbdevel@gmail.com <vmbdevel@gmail.com@c28900bf-92f6-52db-cff4-6a84daa2c767>
2013-08-05 14:27:39 +0000

    test service was added

---
+++ trunk/test/liferay-plugins-sdk-6.1.1/portlets/jengine-test-portlet/docroot/WEB-INF/service/com/jenginetest/builder/service/TestServiceUtil.java
@@ -0,0 +1,95 @@
+/**
+ * Copyright (c) 2000-2012 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.jenginetest.builder.service;
+
+import com.liferay.portal.kernel.bean.PortletBeanLocatorUtil;
+import com.liferay.portal.kernel.util.ReferenceRegistry;
+import com.liferay.portal.service.InvokableService;
+
+/**
+ * The utility for the test remote service. This utility wraps {@link com.jenginetest.builder.service.impl.TestServiceImpl} and is the primary access point for service operations in application layer code running on a remote server.
+ *
+ * <p>
+ * This is a remote service. Methods of this service are expected to have security checks based on the propagated JAAS credentials because this service can be accessed remotely.
+ * </p>
+ *
+ * @author Brian Wing Shun Chan
+ * @see TestService
+ * @see com.jenginetest.builder.service.base.TestServiceBaseImpl
+ * @see com.jenginetest.builder.service.impl.TestServiceImpl
+ * @generated
+ */
+public class TestServiceUtil {
+	/*
+	 * NOTE FOR DEVELOPERS:
+	 *
+	 * Never modify this class directly. Add custom service methods to {@link com.jenginetest.builder.service.impl.TestServiceImpl} and rerun ServiceBuilder to regenerate this class.
+	 */
+
+	/**
+	* Returns the Spring bean ID for this bean.
+	*
+	* @return the Spring bean ID for this bean
+	*/
+	public static java.lang.String getBeanIdentifier() {
+		return getService().getBeanIdentifier();
+	}
+
+	/**
+	* Sets the Spring bean ID for this bean.
+	*
+	* @param beanIdentifier the Spring bean ID for this bean
+	*/
+	public static void setBeanIdentifier(java.lang.String beanIdentifier) {
+		getService().setBeanIdentifier(beanIdentifier);
+	}
+
+	public static java.lang.Object invokeMethod(java.lang.String name,
+		java.lang.String[] parameterTypes, java.lang.Object[] arguments)
+		throws java.lang.Throwable {
+		return getService().invokeMethod(name, parameterTypes, arguments);
+	}
+
+	public static void clearService() {
+		_service = null;
+	}
+
+	public static TestService getService() {
+		if (_service == null) {
+			InvokableService invokableService = (InvokableService)PortletBeanLocatorUtil.locate(ClpSerializer.getServletContextName(),
+					TestService.class.getName());
+
+			if (invokableService instanceof TestService) {
+				_service = (TestService)invokableService;
+			}
+			else {
+				_service = new TestServiceClp(invokableService);
+			}
+
+			ReferenceRegistry.registerReference(TestServiceUtil.class,
+				"_service");
+		}
+
+		return _service;
+	}
+
+	/**
+	 * @deprecated
+	 */
+	public void setService(TestService service) {
+	}
+
+	private static TestService _service;
+}
