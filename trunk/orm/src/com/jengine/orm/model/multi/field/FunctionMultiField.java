db57f157438e7d58b237d0439a73a933ba130b50
vmbdevel@gmail.com <vmbdevel@gmail.com@c28900bf-92f6-52db-cff4-6a84daa2c767>
2014-04-06 14:23:49 +0000

    copyright was added to all files

--- trunk/orm/src/com/jengine/orm/model/multi/field/FunctionMultiField.java
+++ trunk/orm/src/com/jengine/orm/model/multi/field/FunctionMultiField.java
@@ -1,3 +1,22 @@
+/*
+ *  This file is part of JEngine.
+ *  *
+ *  * Copyright (C) 2013-2014 Cetus (http://cs1.com.ua/). All rights reserved.
+ *  *
+ *  * JEngine is free software: you can redistribute it and/or modify
+ *  * it under the terms of the GNU General Public License as published by
+ *  * the Free Software Foundation, either version 3 of the License, or
+ *  * (at your option) any later version.
+ *  *
+ *  * JEngine is distributed in the hope that it will be useful,
+ *  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  * GNU General Public License for more details.
+ *  *
+ *  * You should have received a copy of the GNU General Public License
+ *  * along with JEngine.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.jengine.orm.model.multi.field;
 
 import com.jengine.orm.model.field.Field;
