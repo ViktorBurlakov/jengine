386aa3c04621bcd5a5e9bc74fd3c533b2f98536f
vmbdevel@gmail.com <vmbdevel@gmail.com@c28900bf-92f6-52db-cff4-6a84daa2c767>
2014-04-16 15:12:52 +0000

    some liferay classes were added

--- utils/src/com/jengine/utils/ClassUtils.java
+++ utils/src/com/jengine/utils/ClassUtils.java
@@ -20,8 +20,8 @@
 package com.jengine.utils;
 
 
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 import java.lang.reflect.ParameterizedType;
 import java.util.HashMap;
 import java.util.Map;
@@ -96,5 +96,25 @@ public class ClassUtils {
     @SuppressWarnings("unchecked")
     public static Class getGenericSuperclassType(Class cls, int index) {
         return (Class) ((ParameterizedType) cls.getGenericSuperclass()).getActualTypeArguments()[index];
+    }
+
+    public static boolean isGetter(Method method) {
+        if (Modifier.isPublic(method.getModifiers()) &&
+                method.getParameterTypes().length == 0) {
+            if (method.getName().matches("^get[A-Z].*") &&
+                    !method.getReturnType().equals(void.class))
+                return true;
+            if (method.getName().matches("^is[A-Z].*") &&
+                    method.getReturnType().equals(boolean.class))
+                return true;
+        }
+        return false;
+    }
+
+    public static boolean isSetter(Method method) {
+        return Modifier.isPublic(method.getModifiers()) &&
+                method.getReturnType().equals(void.class) &&
+                method.getParameterTypes().length == 1 &&
+                method.getName().matches("^set[A-Z].*");
     }
 }
